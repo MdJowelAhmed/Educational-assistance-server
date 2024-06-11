d6c7ad3d2c13019aa1a12f993d41d49e04d8538d
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-11 06:30:06 +0600

    added view page functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const app=express()
 const port=process.env.PORT || 5000;
 
@@ -12,7 +14,7 @@ app.use(express.json())
 // mongodb code 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ma7e2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -29,8 +31,33 @@ async function run() {
   try {
     
     const scholarshipCollection=client.db('scholarshipDB').collection('scholarship')
+    const usersCollection=client.db('scholarshipDB').collection('users')
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+       // middlewares 
+       const verifyToken = (req, res, next) => {
+        // console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
+  
+      // auth related api
+      app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
+      })
+
     app.post('/scholarship',async(req,res)=>{
       const scholarship=req.body
       const result=await scholarshipCollection.insertOne(scholarship)
@@ -43,37 +70,25 @@ async function run() {
     })
 
     // user api 
-    app.put('/user', async (req, res) => {
-      const user = req.body
-
-      const query = { email: user?.email }
-      // check if user already exists in db
-      const isExist = await usersCollection.findOne(query)
-      if (isExist) {
-        if (user.status === 'Requested') {
-          // if existing user try to change his role
-          const result = await usersCollection.updateOne(query, {
-            $set: { status: user?.status },
-          })
-          return res.send(result)
-        } else {
-          // if existing user login again
-          return res.send(isExist)
-        }
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-      // save user for the first time
-      const options = { upsert: true }
-      const updateDoc = {
-        $set: {
-          ...user,
-          timestamp: Date.now(),
-        },
-      }
-      const result = await usersCollection.updateOne(query, updateDoc, options)
-    
+    app.get('/detailsScholarship/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await scholarshipCollection.findOne(query)
       res.send(result)
     })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
