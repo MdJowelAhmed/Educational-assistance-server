0bfcf057f88997861efcb945b021eb46896c7342
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-16 07:10:10 +0600

    added manage users api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 // middleWere
 app.use(cors())
@@ -12,9 +14,6 @@ app.use(express.json())
 
 
 // mongodb code 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ma7e2wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -32,7 +31,8 @@ async function run() {
 
     const scholarshipCollection = client.db('scholarshipDB').collection('scholarship')
     const usersCollection = client.db('scholarshipDB').collection('users')
-    const paymentCollection = client.db("scholarshipDB").collection("payments");
+    const paymentCollection = client.db('scholarshipDB').collection('payments')
+    // const paymentCollection = client.db("scholarshipDB").collection("payments");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
@@ -83,6 +83,29 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+
+    app.patch('/users/moderator/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'moderator'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     app.get('/detailsScholarship/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
@@ -90,23 +113,27 @@ async function run() {
       res.send(result)
     })
 
+    // payment create 
     app.post('/create-payment-intent', async (req, res) => {
-      const totalFee = req.body.totalFee
-      const totalFeeInCent = parseFloat(totalFee) * 100
-      if (!totalFee || totalFeeInCent < 1) return
-      // generate clientSecret
-      const { client_secret } = await stripe.paymentIntents.create({
-        amount: totalFeeInCent,
+      const {totalFee} = req.body
+      const amount = parseInt(totalFee * 100)
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
         currency: 'usd',
-        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
-        automatic_payment_methods: {
-          enabled: true,
-        },
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
       })
-      // send client secret as response
-      res.send({ clientSecret: client_secret })
     })
 
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
