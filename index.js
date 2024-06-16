7e84faac59c755c6e36e7ab8a38133b0f7e4a6c2
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-16 19:43:53 +0600

    added verify admin and moderator

--- index.js
+++ index.js
@@ -53,6 +53,33 @@ async function run() {
       })
     }
 
+     // verify admin middleware
+     const verifyAdmin = async (req, res, next) => {
+      console.log('hello')
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      console.log(result?.role)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access!!' })
+
+      next()
+    }
+
+     // verify moderator middleware
+     const verifyModerator = async (req, res, next) => {
+      console.log('hello')
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      console.log(result?.role)
+      if (!result || result?.role !== 'moderator') {
+        return res.status(401).send({ message: 'unauthorized access!!' })
+      }
+
+      next()
+    }
+
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body;
